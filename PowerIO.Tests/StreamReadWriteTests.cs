ae9b1164d6a77cfc060cb2b590baeef46e35d2d9
Yaniv <yaniv.mental@gmail.com>
2021-01-20 21:30:29 +0200

    Finishing basic read stream operations

---
+++ PowerIO.Tests/StreamReadWriteTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace PowerIO.Tests
+{
+   
+    class DemoTest
+    {
+
+        [Test]
+        public void FirstTest()
+        {
+
+            Console.WriteLine("yes");
+            Assert.Pass("Passed first test successfully.");
+        }
+    }
+}
---
+++ PowerIO/StreamReadExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+
+namespace PowerIO
+{
+    public static class StreamReadExtensions
+    {
+        /// <summary>
+        /// Read the given amount of bytes from the stream and return it as a byte array, Blocking until the amount was readed from the stream
+        /// </summary>
+        /// <param name="stream">Stream to read from</param>
+        /// <param name="amount">The amount of bytes to read</param>
+        /// <returns>Byte array with the given length (amount)</returns>
+        public static byte[] ReadByteArray(this Stream stream, int amount)
+        {
+            int currentReadAmount, totalReadAmount = 0;
+            byte[] buffer = new byte[amount];
+            while(amount - totalReadAmount > 0)
+            {
+                currentReadAmount = stream.Read(buffer, totalReadAmount, amount - totalReadAmount);
+                if (currentReadAmount == 0) throw new PowerEndOfStreamException($"Unable to read {{{amount}}} bytes from the stream, only {{{totalReadAmount}}} was readed, successfully", buffer, totalReadAmount);
+                totalReadAmount += currentReadAmount;
+            }
+            return buffer;
+        }
+        /// <summary>
+        /// Read a UInt8 from the given stream
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        public static byte ReadUInt8(this Stream stream) => stream.ReadByteArray(0)[0];
+        public static SByte ReadInt8(this Stream stream) => (SByte)stream.ReadByteArray(0)[0];
+    }
+}
